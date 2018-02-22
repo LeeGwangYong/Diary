89dddf4276a56bae5361564166f06dba833a2fc2
이광용 <rhkdrmfh@gmail.com>
2018-02-23 08:31:09 +0900

    Dynamic TextView

--- Diary/ViewController/InputViewController.swift
+++ Diary/ViewController/InputViewController.swift
@@ -12,11 +12,12 @@ import UITextView_Placeholder
 class InputViewController: ViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
     var dateString: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         setViewController()
-        
+        setKeyboardSetting()
     }
     
     override func setViewController() {
@@ -28,13 +29,45 @@ class InputViewController: ViewController {
         }
         self.textView.placeholder = "오늘은 어떤 기억을 담아둘까?"
         self.textView.tintColor = UIColor(red: 168/255, green: 128/255, blue: 177/255, alpha: 1)
+        
     }
 
     @objc func selectKeepDay() {
         let nextVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: KeepDayViewController.reuseIdentifier)
         self.hidesBottomBarWhenPushed = true
         self.navigationController?.pushViewController(nextVC, animated: true)
-//        self.hidesBottomBarWhenPushed = false
-        
+    }
+}
+
+extension InputViewController {
+    func setKeyboardSetting() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.kyeboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.kyeboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func kyeboardWillShow(_ notification: Notification)
+    {
+        if let keyboadSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        {
+//            adjustKeyboardDismissTapGesture(isKeyboardVisible: true)
+            self.textViewBottomConstraint.constant = keyboadSize.height
+            
+            if let animationDuration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval{
+                UIView.animate(withDuration: animationDuration, animations: { self.view.layoutIfNeeded()})
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    @objc func kyeboardWillHide(_ notification: Notification)
+    {
+        if (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue != nil
+        {
+            self.textViewBottomConstraint.constant = 32
+            
+            if let animationDuration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval{
+                UIView.animate(withDuration: animationDuration, animations: { self.view.layoutIfNeeded()})
+            }
+            self.view.layoutIfNeeded()
+        }
     }
 }
