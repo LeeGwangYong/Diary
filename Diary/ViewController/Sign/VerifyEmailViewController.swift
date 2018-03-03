00f085826e36acdd5d1c8a12ede309aafacae0d3
clapwatermelon <clapwatermelon@gmail.com>
2018-03-03 13:45:26 +0900

    fixed VerifyEmailViewController

--- Diary/ViewController/Sign/VerifyEmailViewController.swift
+++ Diary/ViewController/Sign/VerifyEmailViewController.swift
@@ -13,10 +13,6 @@ import Toast_Swift
 class VerifyEmailViewController: ViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-//    @IBOutlet weak var authCode01: UITextField!
-//    @IBOutlet weak var authCode02: UITextField!
-//    @IBOutlet weak var authCode03: UITextField!
-//    @IBOutlet weak var authCode04: UITextField!
     @IBOutlet weak var completeButton: UIButton!
     @IBOutlet weak var resendEmailButton: UIButton!
     @IBOutlet weak var codeTextField1: CustomTextField!
@@ -27,14 +23,20 @@ class VerifyEmailViewController: ViewController {
                                                   self.codeTextField2,
                                                   self.codeTextField3,
                                                   self.codeTextField4]
-    
-
-    var activeAccountFlag: String = ""
-
+  
     var userIdx: Int!
     var email: String!
     var password: String!
     var nickname = UserDefaults.standard.string(forKey: "nickname")
+    var code: String {
+        var str = ""
+        for field in self.codeTextFields {
+            if let text = field.text {
+                str += text
+            }
+        }
+        return str
+    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -49,16 +51,10 @@ class VerifyEmailViewController: ViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-//        authCode01.delegate = self
-//        authCode02.delegate = self
-//        authCode03.delegate = self
-//        authCode04.delegate = self
+
         setViewController()
         drawCompleteButton()
-//        self.codeTextField1.addTarget(self, action: #selector(drawCompleteButton), for: UIControlEvents.editingChanged)
-//        self.codeTextField2.addTarget(self, action: #selector(drawCompleteButton), for: UIControlEvents.editingChanged)
-//        self.codeTextField3.addTarget(self, action: #selector(drawCompleteButton), for: UIControlEvents.editingChanged)
-//        self.codeTextField4.addTarget(self, action: #selector(drawCompleteButton), for: UIControlEvents.editingChanged)
+
         self.completeButton.addTarget(self, action: #selector(completeButtonClicked), for: .touchUpInside)
         resendEmailButton.addTarget(self, action: #selector(resendEmailButtonClicked), for: .touchUpInside)
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
@@ -70,86 +66,34 @@ class VerifyEmailViewController: ViewController {
             field.maxLength = 1
             field.delegate = self
             field.deletableDelegate = self
-            
+            field.keyboardType = .numberPad
+            field.textAlignment = NSTextAlignment.center
         }
+        self.codeTextField1.becomeFirstResponder()
     }
+    
     func setTextLabel() {
         let text = "인증코드를 이메일로\n보냈습니다"
         questionLabel.text = text
         questionLabel.applyGradientWith(startColor: UIColor(red:  101/255, green: 121/255, blue: 151/255, alpha: 1), endColor: UIColor(red: 94/255, green: 37/255, blue: 99/255, alpha: 1))
         
     }
-    /*
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
-    {
-        let allowedCharacters = CharacterSet.decimalDigits
-        let characterSet = CharacterSet(charactersIn: string)
-        return allowedCharacters.isSuperset(of: characterSet)
-    }
-    */
     @objc func drawCompleteButton(){
         completeButton.layer.cornerRadius = 4
-    /*
-        if (codeTextField1.text?.isEmpty)! || (codeTextField2.text?.isEmpty)! || (codeTextField3.text?.isEmpty)! || (codeTextField4.text?.isEmpty)! {
-            completeButton.backgroundColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1.0)
-            completeButton.isEnabled = false
-            print(codeTextField1.text)
-        } else {
-            completeButton.backgroundColor = UIColor(red: 96/255, green: 60/255, blue: 115/255, alpha: 1.0)
-            completeButton.isEnabled = true
-            print("ffff")
-        }
- */
-        completeButton.backgroundColor = UIColor(red: 96/255, green: 60/255, blue: 115/255, alpha: 1.0)
-        completeButton.isEnabled = true
+        completeButton.backgroundColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1.0)
+        completeButton.isEnabled = false
         self.view.addSubview(completeButton)
     }
     
     func authCodeSetup() {
-//        authCode01.tag = 1
-//        authCode02.tag = 2
-//        authCode03.tag = 3
-//        authCode04.tag = 4
-//
-//
-//        authCode01.textAlignment = .center
-//        authCode02.textAlignment = .center
-//        authCode03.textAlignment = .center
-//        authCode04.textAlignment = .center
-//
         codeTextField1.addBorderBottom(height: 2.0, color: UIColor(red: 168/255, green: 128/255, blue: 177/255, alpha: 1.0))
         codeTextField2.addBorderBottom(height: 2.0, color: UIColor(red: 168/255, green: 128/255, blue: 177/255, alpha: 1.0))
         codeTextField3.addBorderBottom(height: 2.0, color: UIColor(red: 168/255, green: 128/255, blue: 177/255, alpha: 1.0))
         codeTextField4.addBorderBottom(height: 2.0, color: UIColor(red: 168/255, green: 128/255, blue: 177/255, alpha: 1.0))
-        
-//        authCode01.addTarget(self, action: #selector(textChanged), for: .editingChanged)
-//        authCode02.addTarget(self, action: #selector(textChanged), for: .editingChanged)
-//        authCode03.addTarget(self, action: #selector(textChanged), for: .editingChanged)
-//        authCode04.addTarget(self, action: #selector(textChanged), for: .editingChanged)
      
     }
-    /*
-    func appendAuthCode() {
-        activeAccountFlag.append(authCode01.text!)
-        activeAccountFlag.append(authCode02.text!)
-        activeAccountFlag.append(authCode03.text!)
-        activeAccountFlag.append(authCode04.text!)
-        print(activeAccountFlag)
-    }
- */
-    /*
-    @objc func textChanged(sender: UITextField) {
-        if (sender.text?.count)! ==  1 {
-            let nextField = sender.superview?.viewWithTag(sender.tag + 1) as UIResponder!
-            nextField?.becomeFirstResponder()
-            if sender.tag == 4 {
-                nextField?.resignFirstResponder()
-            }
-        }
-    }
-    */
+
     @objc func completeButtonClicked() {
-       // appendAuthCode()
         checkAuthCode()
     }
     
@@ -157,7 +101,7 @@ class VerifyEmailViewController: ViewController {
         self.indicatorView.startAnimating()
         let param: Parameters = [
             "idx" : userIdx,
-            "activeAccountCode" : activeAccountFlag
+            "activeAccountCode" : code
         ]
         SignService.getSignData(url: "verifyemail", parameter: param) { (result) in
             switch result {
@@ -219,31 +163,54 @@ class VerifyEmailViewController: ViewController {
 
 extension VerifyEmailViewController: DeletableTextFieldDelegate, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard string.count > 0 else { return true }
+        guard let textField = textField as? CustomTextField else {return true}
+        guard string.count > 0 else { return true}
         let currentText = textField.text ?? ""
         let prospectiveText = (currentText as NSString).replacingCharacters(in: range, with: string)
-        if currentText.count == 1 {
-            switch textField {
-            case self.codeTextField1, self.codeTextField2, self.codeTextField3:
-                textField.resignFirstResponder()
-                self.codeTextFields[textField.tag + 1].becomeFirstResponder()
-                self.codeTextFields[textField.tag + 1].text = string
-            case self.codeTextField4:
-                textField.resignFirstResponder()
-
-            default:
-                break
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        if allowedCharacters.isSuperset(of: characterSet) {
+            if currentText.count == 1 {
+                switch textField {
+                case self.codeTextField1, self.codeTextField2, self.codeTextField3:
+                    textField.resignFirstResponder()
+                    self.codeTextFields[textField.tag + 1].becomeFirstResponder()
+                    self.codeTextFields[textField.tag + 1].text = string
+                    completeButton.backgroundColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1.0)
+                    completeButton.isEnabled = false
+                    if code.count == 4 {
+                        completeButton.backgroundColor = UIColor(red: 96/255, green: 60/255, blue: 115/255, alpha: 1.0)
+                        completeButton.isEnabled = true
+                    }
+                case self.codeTextField4:
+                    
+                    if code.count == 4 {
+                        textField.resignFirstResponder()
+                    }
+                    
+                default:
+                    break
+                }
             }
+            return allowedCharacters.isSuperset(of: characterSet) && prospectiveText.count <= textField.maxLength
         }
-        return prospectiveText.count < 2
+        return false
     }
     
     func textFieldDidSelectDeleteButton(_ textField: UITextField) {
         switch textField {
         case self.codeTextField2, self.codeTextField3, self.codeTextField4:
             self.codeTextFields[textField.tag - 1].becomeFirstResponder()
+        case self.codeTextField1 :
+            self.codeTextField4.becomeFirstResponder()
         default:
             break
         }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if code.count == 4 {
+            completeButtonClicked()
+        }
+        return true
     }
 }
